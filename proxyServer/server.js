c72f0d7bb74462e25bbb5b1da51521b2b84c131b
Chingiz <ecd4gg@gmail.com>
2024-03-13 15:59:34 +0500

    Set up ENV file

--- proxyServer/server.js
+++ proxyServer/server.js
@@ -4,24 +4,23 @@ import fetch from "node-fetch";
 import bodyParser from "body-parser";
 import {Base64} from "js-base64";
 
+require('dotenv').config();
 
 
-const CLIENT_ID = "3667ac15a3e0b621d8d0";
-const CLIENT_SECRET = "5bcaed3ae249e0968aaadcdb9411373975f9ee23";
+const PORT = process.env.PORT || "4000"
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env["CLIENT_SECRET"]
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 
-app.get('/getAccessToken', async function(req, res){
+app.get('/getAccessToken', async function (req, res) {
 
     const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + req.query.code
-    await  fetch('https://github.com/login/oauth/access_token' + params, {
+    await fetch('https://github.com/login/oauth/access_token' + params, {
         method: "POST",
         headers: {
             accept: "application/json"
@@ -35,7 +34,7 @@ app.get('/getAccessToken', async function(req, res){
     })
 })
 
-app.get("/getUserData", async function(req, res){
+app.get("/getUserData", async function (req, res) {
     req.get("Authorization");
     const authorization = Base64.decode(req.get("Authorization"));
     const data = await (await fetch("https://api.github.com/user", {
@@ -96,7 +95,7 @@ app.get('/getUsers', async (req, res) => {
 app.get('/getUser', async (req, res) => {
     try {
         const authorization = Base64.decode(req.get("Authorization"));
-        const url = `https://api.github.com/users${req.query.username ? "/"+req.query.username : ''}`;
+        const url = `https://api.github.com/users${req.query.username ? "/" + req.query.username : ''}`;
         const response = await fetch(url, {
             method: 'GET',
             headers: {
@@ -111,7 +110,7 @@ app.get('/getUser', async (req, res) => {
 });
 app.get("/userUpdate", async (req, res) => {
     try {
-        const { company, name, location } = req.query;
+        const {company, name, location} = req.query;
         const authorization = Base64.decode(req.get("Authorization"));
         const url = "https://api.github.com/user";
         const response = await fetch(url, {
@@ -133,13 +132,13 @@ app.get("/userUpdate", async (req, res) => {
         }
 
         const data = await response.json();
-        res.json({ message: "User updated successfully", data });
+        res.json({message: "User updated successfully", data});
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Error updating user" });
+        res.status(500).json({message: "Error updating user"});
     }
 });
 
-app.listen(4000, function () {
-    console.log("Server is started on port 4000!");
+app.listen(PORT, function () {
+    console.log(`Server is started on port ${PORT}!`);
 })
